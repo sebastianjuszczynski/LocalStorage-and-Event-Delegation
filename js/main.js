6b6b4b44f2eefb2ff8bf778eb7bf0d0d21a5539b
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-08-02 14:58:19 +0100

    function toggle done added

--- js/main.js
+++ js/main.js
@@ -25,5 +25,15 @@ function populateList(items = [], itemsList) {
     }).join("");
 };
 
+function toggleDone(e) {
+    if (!e.target.matches("input")) return;
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem("items", JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
 addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
 populateList(items, itemsList);
