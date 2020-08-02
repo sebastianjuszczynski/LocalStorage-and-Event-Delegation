1e9753be572edfd55702ddfa33c7c020bfadb83b
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-08-02 14:11:51 +0100

    function addItem added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const items = [];
+
+function addItem(e) {
+    e.preventDefault();
+    const text = (this.querySelector("[name=item]")).value;
+    const item = {
+        text,
+        done: false,
+    }
+}
+
+addItems.addEventListener("submit", addItem);
